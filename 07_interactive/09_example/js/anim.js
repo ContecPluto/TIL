5683d262454bb1bd185c5279b4640c84643cb1e5
ContecPluto <gagle637@gmail.com>
2024-01-02 00:29:19 +0900

    Update: 스크롤을 통해 애니메이션을 제어하도록 수정

--- 07_interactive/09_example/js/anim.js
+++ 07_interactive/09_example/js/anim.js
@@ -1,4 +1,4 @@
-// 태양이 자바스크립트로 변경되도록 수정
+// 각각의 엘리먼트들을 변수로 선언
 const sun = document.querySelector('.sun');
 const cloud1 = document.querySelector('.cloud1');
 const cloud2 = document.querySelector('.cloud2');
@@ -8,9 +8,12 @@ const people = document.querySelector('.people');
 const man = document.querySelector('.man');
 const family = document.querySelector('.family');
 
+// 그레디언트 값을 얻기위한 켄버스
 const canvas = document.createElement('canvas');
 const w = 200;
 const h = 50;
+canvas.width = w;
+canvas.height = h;
 const colours = [
     '#b08fcc',
     '#636888',
@@ -32,45 +35,46 @@ ctx.fillStyle = grd;
 ctx.fillRect(0, 0, w, h);
 ctx.fill();
 
-const maxTime = 40000;
-const intervalTime = 50;
-let time = maxTime;
-setInterval(() => {
-    if (time === 0) time = maxTime;
+// 인터렉티브 디자인을 위한 스크롤시 애니메이션
+window.addEventListener('scroll', () => {
+    const bodyInfo = document.body.getBoundingClientRect();
+    const scrollerHeight = document.scrollingElement.scrollHeight - window.innerHeight;
 
-    const timeVal = time ? time / maxTime : 1;
-    if (sun) {
-        const rotateVal = ((360 * timeVal) + 90) * -1;
-        sun.style.transform = `rotate(${rotateVal}deg)`;
-        circle.style.transform = `rotate(${rotateVal + 90}deg)`;
+    // 현재 스크롤 비율
+    const scrollRatio = 1 + bodyInfo.top / scrollerHeight;
 
-        const rotateRatio = (rotateVal + 90) * -1 / 360;
-        const twoRatio = rotateRatio % 0.5 * 2;
-        const fourRatio = rotateRatio % 0.25 * 4;
+    const rotateVal = ((360 * scrollRatio) + 90) * -1;
+    sun.style.transform = `rotate(${rotateVal}deg)`;
+    circle.style.transform = `rotate(${rotateVal + 90}deg)`;
 
-        let sunOpacity;
-        if (0.75 <= rotateRatio && rotateRatio < 1) {
-            // 0 ~ 90
-            sunOpacity = 2.5 - 2 * rotateRatio;
-        } else if (0.5 <= rotateRatio && rotateRatio < 0.75) {
-            // 90 ~ 180
-            sunOpacity = 4 * rotateRatio - 2;
-        } else {
-            sunOpacity = 0;
-        }
+    // 태양의 위치별 비율
+    const rotateRatio = (rotateVal + 90) * -1 / 360;
+    // 태양의 위치별 비율 * 2
+    const twoRatio = rotateRatio % 0.5 * 2;
+    // 태양의 위치별 비율 * 4
+    const fourRatio = rotateRatio % 0.25 * 4;
 
-        sun.style.opacity = sunOpacity;
-        day.style.opacity = sunOpacity;
-        people.style.opacity = sunOpacity;
-
-        cloud1.style.left = `${90 - twoRatio * 90}%`;
-        cloud2.style.left = `${90 - rotateRatio * 90}%`;
-        family.style.left = `${90 - twoRatio * 90}%`;
-        man.style.left = `${90 - fourRatio * 90}%`;
-        
-        const bgColor = ctx.getImageData(w / 2, h * rotateRatio - 0.1, w, h).data;
-        document.body.style.backgroundColor = `rgba(${bgColor[0]}, ${bgColor[1]}, ${bgColor[2]}, ${bgColor[3]})`;
+    let sunOpacity;
+    if (0.75 <= rotateRatio && rotateRatio < 1) {
+        // 0 ~ 90
+        sunOpacity = 2.5 - 2 * rotateRatio;
+    } else if (0.5 <= rotateRatio && rotateRatio < 0.75) {
+        // 90 ~ 180
+        sunOpacity = 4 * rotateRatio - 2;
+    } else {
+        sunOpacity = 0;
     }
 
-    time = time - intervalTime;
-}, intervalTime);
+    sun.style.opacity = sunOpacity;
+    day.style.opacity = sunOpacity;
+    people.style.opacity = sunOpacity;
+
+    cloud1.style.left = `${90 - twoRatio * 90}%`;
+    cloud2.style.left = `${90 - rotateRatio * 90}%`;
+    family.style.left = `${90 - twoRatio * 90}%`;
+    man.style.left = `${90 - fourRatio * 90}%`;
+    
+    // 미리 생성해둔 켄버스에서 색상을 가져옵니다. -0.01 을 하는 이유는 h가 50일경우 rgba(0, 0, 0, 255)를 리턴하기에 이를 방지하기 위함입니다.
+    const bgColor = ctx.getImageData(w / 2, h * rotateRatio - 0.01, w, h).data;
+    document.body.style.backgroundColor = `rgba(${bgColor[0]}, ${bgColor[1]}, ${bgColor[2]}, ${bgColor[3]})`;
+});
